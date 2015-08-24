265c5f60afcf55a627609e205335d776f946855c
Punith-Mitha <u.punithmithra@gmail.com>
2015-08-24 15:12:06 +0530

    this is the major chnage in the app

--- Project/test/example.js
+++ Project/test/example.js
@@ -1,62 +1,137 @@
-function Table(rows, cols) {
+function main(row, col) {
 
-    count = 0;
-    var display = new Result();
+    var _result = new Result();
+    Table(row, col);
 
-    var table = document.createElement("TABLE");
-    var row1 = document.createElement("TR");
-    var col1 = document.createElement("TD");
-    col1.setAttribute("colspan", 3);
-    col1.appendChild(display.display);
-    row1.appendChild(col1);
-    table.appendChild(row1);
-    //table.setAttribute("border", 1);
-    for (i = 0; i < rows; i++) {
-        var row = document.createElement("TR");
-        for (j = 0; j < cols; j++) {
-            count++;
-            var col = document.createElement("TD");
+    function Table(rows, cols) {
 
-            var btn = new Button(display, count);
-            //var t = document.createTextNode(count); // Create a text node    
-            //btn.appendChild(t);
-            //btn.className = "button black";
-            col.appendChild(btn.value);
-            row.appendChild(col);
+        count = 0;
+        // var display = new Result();
+
+        var table = document.createElement("TABLE");
+        var row1 = document.createElement("TR");
+        var col1 = document.createElement("TD");
+        col1.setAttribute("colspan", 3);
+        col1.appendChild(_result.display);
+        row1.appendChild(col1);
+        table.appendChild(row1);
+        //table.setAttribute("border", 1);
+        for (i = 0; i < rows; i++) {
+            var row = document.createElement("TR");
+            for (j = 0; j < cols; j++) {
+                count++;
+                var col = document.createElement("TD");
+
+                var btn = new Button(count);
+
+                col.appendChild(btn.btn);
+                row.appendChild(col);
+            }
+            table.appendChild(row);
         }
-        table.appendChild(row);
+
+
+        var row1 = document.createElement("TR");
+
+
+
+        var col1 = document.createElement("TD");
+        var btn = new cancel("C", "button orange");
+        col1.appendChild(btn.btn);
+        row1.appendChild(col1);
+
+        var col1 = document.createElement("TD");
+        var btn = new Button("0");
+        col1.appendChild(btn.btn);
+        row1.appendChild(col1);
+
+
+        var col1 = document.createElement("TD");
+        var btn = new equals("=", "button orange");
+        col1.appendChild(btn.btn);
+        row1.appendChild(col1);
+
+        table.appendChild(row1);
+
+        var row1 = document.createElement("TR");
+
+        var col1 = document.createElement("TD");
+        var btn = new Button("+", "button pink");
+        col1.appendChild(btn.btn);
+        row1.appendChild(col1);
+
+        var col1 = document.createElement("TD");
+        var btn = new Button("-", "button pink");
+        col1.appendChild(btn.btn);
+        row1.appendChild(col1);
+
+        var col1 = document.createElement("TD");
+        var btn = new Button("*", "button pink");
+        col1.appendChild(btn.btn);
+        row1.appendChild(col1);
+
+        table.appendChild(row1);
+
+        document.body.appendChild(table);
+    }
+
+    function Result() {
+        this.number1;
+        this.number2;
+        this.result;
+        this.display = document.createElement("INPUT")
+        this.display.setAttribute("type", "text");
+        this.resultObj = function () {
+            return this;
+        };
+    }
+
+    function Button(value, cls) {
+
+        this.btn = document.createElement("BUTTON");
+        var t = document.createTextNode(value); // Create a text node    
+        this.btn.appendChild(t);
+        this.btn.className = cls || "button black";
+        this.btn.value = value;
+        //this.btn.setAttribute("onclick", click);
+        this.btn.onclick = function (value) {
+            //alert(this.value);
+            _result.display.value += this.value;
+
+        }
+
+        return this;
+
+    }
+
+    function equals(value, cls) {
+        this.btn = document.createElement("BUTTON");
+        var t = document.createTextNode("="); // Create a text node    
+        this.btn.appendChild(t);
+        this.btn.className = cls || "button black";
+        // this.btn.value = value;
+        this.btn.onclick = function (value) {
+            //alert(this.value);
+            _result.display.value = eval(_result.display.value);
+
+        }
+
+    }
+
+    function cancel(value, cls) {
+        this.btn = document.createElement("BUTTON");
+        var t = document.createTextNode(value); // Create a text node    
+        this.btn.appendChild(t);
+        this.btn.className = cls || "button black";
+        // this.btn.value = value;
+        this.btn.onclick = function (value) {
+            //alert(this.value);
+            _result.display.value = "";
+
+        }
+
     }
 
 
-
-    document.body.appendChild(table);
+    Button.prototype = new Result();
 }
-
-function Result() {
-    this.number1;
-    this.number2;
-    this.result;
-    this.display = document.createElement("INPUT")
-    this.display.setAttribute("type", "text");
-    document.body.appendChild(this.display);
-}
-
-function Button(type, value) {
-
-    this.value = document.createElement("BUTTON");
-    var t = document.createTextNode(count); // Create a text node    
-    this.value.appendChild(t);
-    this.value.className = "button black";
-    this.type = type;
-
-    this.setnum1 = function (value, display) {
-
-        return display += value;
-
-    }
-
-    return this;
-
-}
-
-Button.prototype = new Result();
